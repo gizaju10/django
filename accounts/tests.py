2ff327176907c38725d488445b9aa55165654ed8
gizaju10 <kaminomi3@gmail.com>
2020-07-16 05:48:03 +0900

    tests.pyの利用開始 テスト用ユーザーでログインまで

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse_lazy
 
-# Create your tests here.
+from .models import User, UserManager
+
+
+class LoggedInTestCase(TestCase):
+    def setUp(self):
+        """テストメソッド実行前の事前設定"""
+
+        # テストユーザーのパスワード
+        self.password = 'pass1122'
+
+        # テスト用ユーザーを生成しインスタンス変数に格納しておく
+        self.test_user = get_user_model().objects.create_user(username='Guest_user', password=self.password)
+
+        # テスト用ユーザーでログインする
+        self.client.login(email=self.test_user.email, password=self.password)
